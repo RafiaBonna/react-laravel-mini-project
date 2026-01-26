a5fa6bf7977bc49ca2fed39c63d557643e75fa08
RafiaBonna <rafiaakterbonna@gmail.com>
2026-01-26 23:50:28 +0600

    my

--- my-backend-api/app/Models/User.php
+++ my-backend-api/app/Models/User.php
@@ -13,7 +13,7 @@ class User extends Authenticatable
     use HasFactory, Notifiable;
 
     /**
-     * The attributes that are mass assignables...
+     * The attributes that are mass assignables....
      *
      * @var list<string>
      */
