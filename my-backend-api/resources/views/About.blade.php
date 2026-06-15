3c9ebaf2ad87cb5a74399b44ddd37efdcd2f9df6
RafiaBonna <rafiaakterbonna@gmail.com>
2026-06-16 00:49:01 +0600

    laravel

--- my-backend-api/resources/views/About.blade.php
+++ my-backend-api/resources/views/About.blade.php
@@ -5,13 +5,13 @@
     <title>About Our App</title>
     <style>
         body { font-family: sans-serif; text-align: center; padding: 50px; background: #f4f4f4; }
-        .card { background: white; padding: 22px; border-radius: 10px; display: inline-block; box-shadow: 0 4px 8px rgba(0,0,0,0.1); }
+        .card { background: white; padding: 20px; border-radius: 10px; display: inline-block; box-shadow: 0 4px 8px rgba(0,0,0,0.1); }
         h1 { color: #ff2d20; }
     </style>
 </head>
 <body>
     <div class="card">
-        <h1>About This Project :</h1>
+        <h1>About This Project</h1>
         <p>This is a <strong>Laravel + React</strong> CRUD Application</p>
         <p>Current Step:Building the UI & API</p>
         <a href="/">Go Back Home</a>
